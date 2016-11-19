e0ed47bcf1015d156fd2a93d77cd2e43886a1dbf
Geek-1001 <demhasul@gmail.com>
2016-11-19 23:56:50 +0200

    Add Video View Controller to control video playback and all additional video file setup

---
+++ Clue/Clue/Classes/ViewControllers/CLUVideoViewController.m
@@ -0,0 +1,24 @@
+//
+//  CLUVideoViewController.m
+//  Clue
+//
+//  Created by Ahmed Sulaiman on 11/5/16.
+//  Copyright © 2016 Ahmed Sulaiman. All rights reserved.
+//
+
+#import "CLUVideoViewController.h"
+
+@interface CLUVideoViewController ()
+
+@end
+
+@implementation CLUVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+    [self.view setWantsLayer:YES];
+    [self.view.layer setBackgroundColor:[NSColor redColor].CGColor];
+}
+
+@end
