3ac441216972e3017f0381b5fea77be1aa3da8f5
Geek-1001 <demhasul@gmail.com>
2016-12-03 19:31:36 +0200

    Subclass Video View Controller from Root View Controller (to get access to common functions for every view controller)

--- Clue/Clue/Classes/ViewControllers/CLUVideoViewController.h
+++ Clue/Clue/Classes/ViewControllers/CLUVideoViewController.h
@@ -8,7 +8,8 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "CLURootViewController.h"
 
-@interface CLUVideoViewController : NSViewController
+@interface CLUVideoViewController : CLURootViewController
 
 @end
