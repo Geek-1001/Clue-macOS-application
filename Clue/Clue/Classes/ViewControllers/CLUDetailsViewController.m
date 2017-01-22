7ff78e35f980590f29bde4f37fb697a1010712b9
Geek-1001 <demhasul@gmail.com>
2017-01-23 00:56:23 +0200

    Add to Details View Controller tabs name with specified type, switch between view controllers by changing selected tab (for now only Device Info View Controller), Add two separators for details section

--- Clue/Clue/Classes/ViewControllers/CLUDetailsViewController.m
+++ Clue/Clue/Classes/ViewControllers/CLUDetailsViewController.m
@@ -9,11 +9,13 @@
 #import "CLUDetailsViewController.h"
 #import "KPCTabsControl/KPCTabsControl-Swift.h"
 #import "CLUTabBarItem.h"
+#import "CLUDeviceInfoViewController.h"
 
-@interface CLUDetailsViewController () <TabsControlDataSource>
+@interface CLUDetailsViewController () <TabsControlDataSource, TabsControlDelegate>
 
 @property (weak) IBOutlet TabsControl *tabsControl;
 @property (nonatomic) NSArray *tabControlItems;
+@property (weak) IBOutlet CLUDeviceInfoViewController *deviceInfoViewController;
 
 @end
 
@@ -26,10 +28,10 @@
     if (!self) {
         return nil;
     }
-    _tabControlItems = @[[[CLUTabBarItem alloc] initWithTitle:@"Network"],
-                         [[CLUTabBarItem alloc] initWithTitle:@"User Interactions"],
-                         [[CLUTabBarItem alloc] initWithTitle:@"View Structure"],
-                         [[CLUTabBarItem alloc] initWithTitle:@"Device Info"]];
+    _tabControlItems = @[[[CLUTabBarItem alloc] initWithTitle:@"Network" forType:CLUTabBarItemNetwork],
+                         [[CLUTabBarItem alloc] initWithTitle:@"User Interactions" forType:CLUTabBarItemUserInetraction],
+                         [[CLUTabBarItem alloc] initWithTitle:@"View Structure" forType:CLUTabBarItemViewStructure],
+                         [[CLUTabBarItem alloc] initWithTitle:@"Device Info" forType:CLUTabBarItemDeviceInfo]];
     return self;
 }
 
@@ -37,19 +39,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupSeparators];
     
     ClueStyle *style = [[ClueStyle alloc] init];
     style.tabButtonWidth = TabWidthFlexible;
     
     _tabsControl.dataSource = self;
+    _tabsControl.delegate = self;
     _tabsControl.style = style;
     [_tabsControl reloadTabs];
+    [_tabsControl selectItemAtIndex:0];
+}
+
+// TODO: refactor. Add searators from Interface Builder
+- (void)setupSeparators {
+    NSView *verticalSeparator = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, 1, self.view.frame.size.height)];
+    [verticalSeparator setWantsLayer:YES];
+    verticalSeparator.layer.backgroundColor = [[NSColor colorWithRed:24/255.0 green:24/255.0 blue:24/255.0 alpha:1] CGColor];
+    [self.view addSubview:verticalSeparator];
+    NSView *horizontalSeparator = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, self.view.frame.size.width, 1)];
+    [horizontalSeparator setWantsLayer:YES];
+    horizontalSeparator.layer.backgroundColor = [[NSColor colorWithRed:24/255.0 green:24/255.0 blue:24/255.0 alpha:1] CGColor];
+    [self.view addSubview:horizontalSeparator];
 }
 
 #pragma mark - Tabs Control Data Source
 
 - (NSInteger)tabsControlNumberOfTabs:(TabsControl * _Nonnull)control {
-    return 4;
+    return _tabControlItems.count;
 }
 
 - (CLUTabBarItem *)tabsControl:(TabsControl * _Nonnull)control itemAtIndex:(NSInteger)index {
@@ -58,6 +75,23 @@
 
 - (NSString * _Nonnull)tabsControl:(TabsControl * _Nonnull)control titleForItem:(CLUTabBarItem *)item {
     return item.title;
+}
+
+#pragma mark - Tabs Control Delegate
+
+- (void)tabsControlDidChangeSelection:(TabsControl *)control item:(CLUTabBarItem *)item {
+    // Hide everything
+    [_deviceInfoViewController.view setHidden:YES];
+    
+    // Show only selected
+    switch (item.type) {
+        case CLUTabBarItemDeviceInfo:
+            [_deviceInfoViewController.view setHidden:NO];
+            break;
+            
+        default:
+            break;
+    }
 }
 
 @end
