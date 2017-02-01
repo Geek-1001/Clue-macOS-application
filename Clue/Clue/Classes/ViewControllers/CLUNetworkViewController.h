e10c05a4ac41f91c9c6dee72dfa84a21769eee49
Geek-1001 <demhasul@gmail.com>
2017-02-02 01:07:44 +0200

    Add Network View Controller to represent Network data model in Outline View (a lot of simlaroty here with View Structure View Controller, probably I'll make some class in-between RootViewController and time related modules like network, view structure and user interactions)

---
+++ Clue/Clue/Classes/ViewControllers/CLUNetworkViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLUNetworkViewController.h
+//  Clue
+//
+//  Created by Ahmed Sulaiman on 1/31/17.
+//  Copyright © 2017 Ahmed Sulaiman. All rights reserved.
+//
+
+#import "CLURootViewController.h"
+
+@interface CLUNetworkViewController : CLURootViewController
+
+@end
