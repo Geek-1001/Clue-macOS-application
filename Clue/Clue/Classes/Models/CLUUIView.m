5546a774c839fd4af69c5d0b32d3b34cf36d99f2
Geek-1001 <demhasul@gmail.com>
2017-04-12 23:59:00 +0300

    Fix typo in CLUUIView classNameString:containsOrEqualTo: method and change some variables names to more convenient ones.

--- Clue/Clue/Classes/Models/CLUUIView.m
+++ Clue/Clue/Classes/Models/CLUUIView.m
@@ -64,11 +64,15 @@
     if (!jsonDictionary || !subviewsArray) {
         return;
     }
-    
     NSArray<NSDictionary *> *subviewsJSONArray = [jsonDictionary objectForKey:SUBVIEWS_KEY];
+    if (!subviewsJSONArray) {
+        return;
+    }
     for (NSDictionary *subviewsJSONItem in subviewsJSONArray) {
         CLUUIView *subview = [[CLUUIView alloc] initWithJSONRepresentation:subviewsJSONItem];
-        [subviewsArray addObject:subview];
+        if (subview) {
+            [subviewsArray addObject:subview];
+        }
     }
 }
 
@@ -92,35 +96,36 @@
 
 // TODO: refactor. Put all UIKit class names into object/enum to remove string check
 - (CLUOutlineViewDataItemType)itemType {
-    if ([self classNameString:_className containsOfEquestTo:@"UILabel"]) {
+    if ([self classNameString:_className containsOrEqualTo:@"UILabel"]) {
         return CLUOutlineViewDataUILabelItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UIButton"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UIButton"]) {
         return CLUOutlineViewDataUIButtonItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UIImageView"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UIImageView"]) {
         return CLUOutlineViewDataUIImageViewItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UISearchView"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UISearchView"]) {
         return CLUOutlineViewDataUISearchViewItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UITableView"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UITableView"]) {
         return CLUOutlineViewDataUITableViewItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UITextField"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UITextField"]) {
         return CLUOutlineViewDataUITextFieldItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UIWindow"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UIWindow"]) {
         return CLUOutlineViewDataUIWindowItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UIWebView"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UIWebView"]) {
         return CLUOutlineViewDataUIWebViewItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UISwitch"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UISwitch"]) {
         return CLUOutlineViewDataUISwitchItem;
-    } else if ([self classNameString:_className containsOfEquestTo:@"UISlider"]) {
+    } else if ([self classNameString:_className containsOrEqualTo:@"UISlider"]) {
         return CLUOutlineViewDataUISliderItem;
     }
     return CLUOutlineViewDataUIViewItem;
 }
 
-- (BOOL)classNameString:(NSString *)className containsOfEquestTo:(NSString *)classToCheck {
-    if (!classToCheck || classToCheck.length < 2 || !className) {
+- (BOOL)classNameString:(NSString *)className containsOrEqualTo:(NSString *)classNameToCheck {
+    if (!classNameToCheck || classNameToCheck.length < 2 || !className) {
         return NO;
     }
-    return [className containsCaseInsensitiveString:[classToCheck substringFromIndex:2]] || [className isEqualToString:classToCheck];
+    return [className containsCaseInsensitiveString:[classNameToCheck substringFromIndex:2]]
+                                                    || [className isEqualToString:classNameToCheck];
 }
 
 @end
